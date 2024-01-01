1711a9183b16c611a0fcc08fee3f2bd20ba0c9ce
JHoldsworth23 <jacobph101@icloud.com>
2024-01-01 14:13:31 +0000

    Add the submit button eventListener to capture all book details from input

--- script.js
+++ script.js
@@ -4,9 +4,16 @@ function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read === "on" ? true : false;
+    this.read = read === 'on' ? true : false;
 }
 
 function addBookToLibrary() {
     // do stuff here...
 }
+
+const submitBtn = document.querySelector('button');
+submitBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    const bookDetails = Array.from(document.querySelectorAll('input'));
+    console.log(bookDetails)
+});
