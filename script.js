ca16e6ecd1042d34ab3c82a04e30a193c641c1fd
JHoldsworth23 <jacobph101@icloud.com>
2024-01-02 19:13:40 +0000

    Check if the form is valid and reset the input value when a user submit

--- script.js
+++ script.js
@@ -4,12 +4,17 @@ function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read === 'on' ? true : false;
+    this.read = read;
 }
 
 function addBookToLibrary(title, author, pages, read) {
     const newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
+}
+
+function clearForm() {
+    const allInputs = document.querySelectorAll('input');
+    allInputs.forEach(input => console.log(input));
 }
 
 function getBookInfo(event) {
@@ -19,8 +24,12 @@ function getBookInfo(event) {
     const bookPages = document.querySelector('#pages');
     const bookRead = document.querySelector('input[type*="box"]');
 
-    addBookToLibrary(bookTitle.value, bookAuthor.value, bookPages.value, bookRead.value);
+    if (form.checkValidity()) {
+        addBookToLibrary(bookTitle.value, bookAuthor.value, bookPages.value, bookRead.checked);
+        clearForm();
+    }
 }
 
 const submitBtn = document.querySelector('button');
+const form = document.querySelector('form');
 submitBtn.addEventListener('click', getBookInfo);
