f4d3dce8435a93c19ffe4e78d7e7e6275cc5460c
JHoldsworth23 <jacobph101@icloud.com>
2024-01-14 20:05:09 +0000

    Add the curly braces in Book class to have default params value

--- updated-script.js
+++ updated-script.js
@@ -1,6 +1,6 @@
 // Rewrite these code with the use of Class
 class Book {
-    constructor(title = 'Unknown', author = 'Unknown', pages = '0', read = false) {
+    constructor({ title = 'Unknown', author = 'Unknown', pages = '0', read = false }) {
         this.title = title;
         this.author = author;
         this.pages = pages;
