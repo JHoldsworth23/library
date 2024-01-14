efe19de3c6e1e8b5b5d519cdde0d49e9b54a5c05
JHoldsworth23 <jacobph101@icloud.com>
2024-01-14 19:58:35 +0000

    Add method in library class to display books in the grid

--- updated-script.js
+++ updated-script.js
@@ -16,6 +16,26 @@ class Library {
     addBookToLibrary(newBook) {
         this.myLibrary.push(newBook);
     }
+
+    displayBooks(panel) {
+        panel.innerHTML = '';
+        if (this.myLibrary.length !== 0) {
+            this.myLibrary.forEach(book => {
+               const div = document.createElement('div');
+               div.classList.add('book-card');
+               div.innerHTML =  `
+                   <p class="book-title">${book.title.toUpperCase()} <br><span>by</span></p>
+                   <p class="book-author">${book.author}</p>
+                   <p>${book.pages} pages</p>
+                   <div>
+                       <button class="read">${book.read ? '<i class="fa-solid fa-book-bookmark"></i> Read' : '<i class="fa-solid fa-book not-read"></i> Not Read'}</button>
+                       <button class="delete"><i class="fa-solid fa-trash"></i></button>
+                   </div> 
+               `;
+               panel.append(div);
+            });
+        }
+    }
 }
 
 const library = new Library();
@@ -25,6 +45,7 @@ const form = document.querySelector('form');
 const dialog = document.querySelector('dialog');
 const submitBtn = document.querySelector('#submit-btn');
 const cancelBtn = document.querySelector('#cancel-btn');
+const grid = document.querySelector('.grid-container');
 
 const getBookInfo = () => {
     const bookTitle = document.querySelector('#title').value;
@@ -41,6 +62,7 @@ const addBook = (event) => {
     library.addBookToLibrary(newBook);
     form.reset();
     dialog.close();
+    library.displayBooks(grid);
 }
 
 showForm.addEventListener('click', () => {
